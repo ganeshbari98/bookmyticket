dd598151699b434bf70a814413ec2e7432ef2d46
admin <ganeshbari98@gmail.com>
2026-05-01 13:48:31 +0530

    new

--- src/main/java/com/example/bookmyticket/Bus.java
+++ src/main/java/com/example/bookmyticket/Bus.java
@@ -1,4 +1,15 @@
 package com.example.bookmyticket;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Bus {
+
+    @GetMapping("/Bus")
+    public String getData(){
+
+        return "Welcome to BookMyPlan, Enjoy your holidays booking at 100% off";
+    }
+
 }
